a94d04b4bd16144fb3d3da408c02be1474783ec3
tylerthomasallen <tylerthomasallen@gmail.com>
2020-10-22 16:07:55 -0700

    add test

---
+++ src/helper.js
@@ -0,0 +1,4 @@
+export const double = (number) => {
+  return number * 2;
+}
+
---
+++ src/helper.test.js
@@ -0,0 +1,7 @@
+import { double } from './helper';
+
+describe('helper tests', () => {
+  it('doubles a number', () => {
+    expect(double(2)).toEqual(4)
+  })
+})
